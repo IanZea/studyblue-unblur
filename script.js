abaec29bf319ced951adb6b4c918e868fcd515a9
Jacob Jordan <JORDAN_J@afi.afiama.com>
2016-08-03 13:33:26 -0500

    added \n at end of files, removed screenshot

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+console.log("======================================================================")
 console.log('StudyBlue Unblur! by @JacobAJordan_');
+console.log('Code available at: https://github.com/jacobjordan94/studyblue-unblur');
+console.log("======================================================================")
 $('.card').css('overflow','auto');
 $('.back > .side.side-text > .text.text-small').css('color', 'black');
 $('head').append('<style>#cards .card:after, .cards-wrap .card:after {background: transparent} </style>');
