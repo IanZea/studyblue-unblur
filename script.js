57633831daabc2b6337478dedc8cacfe41ec4487
Jacob Jordan <JORDAN_J@afi.afiama.com>
2016-08-05 10:32:44 -0500

    Cards now automatically resize based on the amount of text in them

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-console.log("======================================================================")
-console.log('StudyBlue Unblur! by @JacobAJordan_');
-console.log('Code available at: https://github.com/jacobjordan94/studyblue-unblur');
-console.log("======================================================================")
-$('.card').css('overflow','auto');
+console.log('======================================================================\n' +
+			'StudyBlue Unblur! by @JacobAJordan_\n' + 
+			'Code available at: https://github.com/jacobjordan94/studyblue-unblur\n' +
+			'======================================================================');
 $('.back > .side.side-text > .text.text-small').css('color', 'black');
 $('head').append('<style>#cards .card:after, .cards-wrap .card:after {background: transparent} </style>');
 $('.card.blur').removeClass('blur');
 $('.card.card-ad').remove();
+$('.card').css('min-height', '200px');
+$('.card').css('max-height', 'auto');
+$('.card').css('height', 'auto');
+$('.card-container').masonry({
+	itemSelector: '.card',
+    columnWidth: 310
+});
