2cb3a940282aae6101b3bd89e71fa5d00ce2dd29
Jacob Jordan <jacobjordan94@live.com>
2017-04-26 21:49:02 -0500

    Updated extension to work with StudyBlue website updates

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-console.log('======================================================================\n' +
-			'StudyBlue Unblur! by @JacobAJordan_\n' + 
-			'Code available at: https://github.com/jacobjordan94/studyblue-unblur\n' +
-			'======================================================================');
-$('.back > .side.side-text > .text.text-small').css('color', 'black');
-$('head').append('<style>#cards .card:after, .cards-wrap .card:after {background: transparent} </style>');
-$('.card.blur').removeClass('blur');
-$('.card.card-ad').remove();
-$('.card').css('min-height', '200px');
-$('.card').css('max-height', 'auto');
-$('.card').css('height', 'auto');
-$('.card-container').masonry({
-	itemSelector: '.card',
-    columnWidth: 310
+console.log('StudyBlue Unblur! by @JacobAJordan_');
+console.log('Source code available at: https://github.com/jacobjordan94/studyblue-unblur');
+
+$(document).ready(function(){
+	$('head').append('<style>#cards .card:after, .cards-wrap .card:after {background: transparent} </style>');
+	$('.modal-wrapper').remove();
+	$('.card').click(function(){
+		$('#seo-landing').removeAttr('style');
+	});
+	setInterval(function(){
+		console.log('Unblurring');
+		hiddenCards = null; // LOL come on guys. 
+		$('.gameOn').css('color', 'black !important');
+		$('.gameOn').css('text-shadow', 'none');
+		$('.gameOn > ul').css('color', 'black');
+		$('.gameOn > ul > li').css('color', 'black');
+		$('.gameOn > div').css('color', 'black');
+		$('.card.card-ad').remove();
+		$('.card').css('min-height', '200px');
+		$('.card').css('max-height', 'auto');
+		$('.card').css('height', 'auto');
+		$('.card-container').masonry({
+			itemSelector: '.card',
+		    columnWidth: 310
+		});
+	}, 1000);
 });
